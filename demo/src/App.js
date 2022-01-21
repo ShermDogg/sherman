4e5ff3dd8dd23de3bfbea57b515a1874772c453d
sherman coe <shermancoe74@gmail.com>
2022-01-21 00:31:45 -0500

    product list creation and homescreen

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
-      <h>SheShares</h>
+    <Header />
+    <main>
+      <Container>
+        <HomeScreen />
+      </Container>
+    </main>
       
+      <Footer />
     </>
   );
 }
